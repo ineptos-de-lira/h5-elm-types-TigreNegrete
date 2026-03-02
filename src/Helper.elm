050ac5ced8b7b54fa635af9e5409aa3f94201f84
Rubén Salvador Negrete Carreón <salvadorruben82@gmail.com>
2026-03-01 18:54:54 -0600

    Homework 5

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,66 @@
 module Helper exposing (..)
+
+
+type MiBool
+    = MiTrue
+    | MiFalse
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade list =
+    case list of
+        [] ->
+            []
+
+        x :: xs ->
+            if x >= 7 then
+                Approved :: categoricalGrade xs
+
+            else if x < 0 then
+                Pending :: categoricalGrade xs
+
+            else
+                Failed :: categoricalGrade xs
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+        Delayed ->
+            "Esperar"
+
+        Cancelled ->
+            "Pedir reembolso"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction list =
+    case list of
+        [] ->
+            []
+
+        x :: xs ->
+            airplaneScheduleAction x :: airportAction xs
+
+
+airplaneStatuses =
+    [ OnTime, Boarding, Delayed, Cancelled ]
